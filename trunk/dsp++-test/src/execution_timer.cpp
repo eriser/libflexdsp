bde9c55280bf8bf9be937f0c0966ade08fb932e1
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-02 12:02:14 +0000

    [dsp++] added execution_timer to dsp++-test, experimenting with SSE microoptimizations (for learning purposes, not that it's really important)

---
+++ trunk/dsp++-test/src/execution_timer.cpp
@@ -0,0 +1,64 @@
+/*
+ * execution_timer.cpp
+ *
+ *  Created on: 31-05-2013
+ *      Author: Andrzej
+ */
+
+#include "execution_timer.h"
+
+#ifdef _WIN32
+# include <windows.h>
+#endif
+
+#include <cstring>
+#include <climits>
+#include <cstdio>
+
+dsp::test::execution_timer::execution_timer():
+#ifdef _WIN32
+	ft_(0)
+#endif
+{
+#ifdef __posix__
+	std::memset(&tv_, 0, sizeof(tv_));
+#endif
+}
+
+void dsp::test::execution_timer::start(const char* run) {
+	run_ = run;
+#ifdef _WIN32
+	ft_ = ::GetTickCount();
+#endif
+
+#ifdef __posix__
+	gettimeofday(&tv_, NULL);
+#endif
+}
+
+void dsp::test::execution_timer::stop() {
+	unsigned long millis = 0;
+#ifdef _WIN32
+	unsigned long now = ::GetTickCount();
+	if (now >= ft_)
+		millis = now - ft_;
+	else
+		millis = (ULONG_MAX - ft_ + now + 1);
+#endif
+
+#ifdef __posix__
+	timeval ntv;
+	gettimeofday(&ntv, NULL);
+	millis = 1000 * (ntv.tv_sec - tv_.tv_sec);
+	millis += (ntv.tv_usec - tv_.tv_usec + 500) / 1000;
+#endif
+
+	printf("\nrun %s time: %lu ms\n", run_.c_str(), millis);
+}
+
+void dsp::test::execution_timer::next(const char* run) {
+	stop();
+	start(run);
+}
+
+
--- trunk/dsp++/src/arch/x86/sse3.cpp
+++ trunk/dsp++/src/arch/x86/sse3.cpp
@@ -9,27 +9,6 @@
 #include <pmmintrin.h>
 
 //! @brief Dot product using SSE3 instruction set.
-float dsp::simd::detail::x86_sse3_dotf(const float* x, const float* b, size_t N)
-{
-	float res = 0.f;
-	__m128 b0, b1, b2, b3, x0, x1, x2, x3;
-	size_t n = N / 16;
-	for (size_t i = 0; i < n; ++i, x += 16, b += 16) {
-		SSE_LOAD16(b, b);
-		SSE_LOAD16(x, x);
-		SSE_MUL16(x, x, b);
-		SSE3_HSUM16(x0, x);
-		res += _mm_cvtss_f32(x0);
-	}
-	n = (N % 16) / 4;
-	for (size_t i = 0; i < n; ++i, b += 4, x += 4) {
-		b0 = _mm_load_ps(b);
-		x0 = _mm_load_ps(x);
-		x0 = _mm_mul_ps(x0, b0);
-		SSE3_HSUM(x0, x0);
-		res += _mm_cvtss_f32(x0);
-	}
-	return res;
-}
+SSE3_SUM_FVVS(dsp::simd::detail::x86_sse3_dotf, mul_ps)
 
 #endif // DSP_ARCH_FAMILY_X86
