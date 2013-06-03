924c584bb9e66128d6b93aa8ffea37ed4df7cb86
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-03 12:55:47 +0000

    [dsp++] work in progress on fixed class

--- dsp++/include/dsp++/fixed.h
+++ dsp++/include/dsp++/fixed.h
@@ -1,28 +1,32 @@
-
+/*!
+ * @file dsp++/fixed.h
+ * @brief Fixed-point number template and associated stuff.
+ */
 #ifndef DSP_FIXED_H_INCLUDED
 #define DSP_FIXED_H_INCLUDED
 #pragma once
 
 #include <dsp++/stdint.h>
 
+#include <limits>
+
 namespace dsp { 
 
 namespace detail {
-
-	template<bool sel, class T0, class T1> struct select_type_if;
-	template<class T0, class T1> struct select_type_if<true, T0, T1> {typedef T0 type;};
-	template<class T0, class T1> struct select_type_if<false, T0, T1> {typedef T1 type;};
-
+	// Implementation details, don't look here (fold down this namespace) ;)
 	template<unsigned bits, bool valid = (bits == 8 || bits == 16 || bits == 32 || bits == 64)> struct fixed_word_length_valid;
 	template<unsigned bits> struct fixed_word_length_valid<bits, true> {};
 
-	template<unsigned bits, unsigned int_bits, bool sign, bool valid = (int_bits + (sign ? 1 : 0) <= bits)> struct int_bits_fit_in_word_length;
-	template<unsigned bits, unsigned int_bits, bool sign> struct int_bits_fit_in_word_length<bits, int_bits, sign, true> {};
+	template<unsigned bits, unsigned int_bits, bool sign, bool valid = (int_bits + (sign ? 1 : 0) <= bits)> struct fixed_int_bits_fit_in_word_length;
+	template<unsigned bits, unsigned int_bits, bool sign> struct fixed_int_bits_fit_in_word_length<bits, int_bits, sign, true> {};
 
-	template<unsigned bits, unsigned int_bits, bool sign> struct fixed_rep: fixed_word_length_valid<bits>, int_bits_fit_in_word_length<bits, int_bits, sign> {
+	template<unsigned bits, unsigned int_bits, bool sign> struct fixed_rep: fixed_word_length_valid<bits>, fixed_int_bits_fit_in_word_length<bits, int_bits, sign> {
 		typedef typename select_int<bits, sign>::type type;
 		static const unsigned fractional_bits = (bits - int_bits - (sign ? 1 : 0));
 	};
+
+	template<class T, unsigned n> struct pow2 {static const T value = 2 * pow2<T, n - 1>::value;};
+	template<class T> struct pow2<T, 0> {static const T value = 0;};
 
 } // namespace detail
 
@@ -30,13 +34,13 @@ namespace detail {
 namespace round {
 	//! @brief Rounding mode.
 	enum mode {
-		fastest,		//!< Speed is more important than the choice in value.
-		negative,		//!< Round towards negative infinity. This mode is useful in interval arithmetic.
-		truncated,		//!< Round towards zero. This mode is useful in implementing integral arithmetic.
-		positive,		//!< Round towards positive infinity. This mode is useful in interval arithmetic.
-		classic,		//!< Round towards the nearest value, but exactly-half values are rounded towards maximum magnitude. This mode is the standard school algorithm.
-		near_even,		//!< Round towards the nearest value, but exactly-half values are rounded towards even values. This mode has more balance than the classic mode.
-		near_odd,		//!< Round towards the nearest value, but exactly-half values are rounded towards odd values. This mode has as much balance as the near_even mode, but preserves more information.
+		fastest = std::round_indeterminate,			//!< Speed is more important than the choice in value.
+		negative = std::round_toward_neg_infinity,	//!< Round towards negative infinity. This mode is useful in interval arithmetic.
+		truncated = std::round_toward_zero,			//!< Round towards zero. This mode is useful in implementing integral arithmetic.
+		positive = std::round_toward_infinity,		//!< Round towards positive infinity. This mode is useful in interval arithmetic.
+		classic = std::round_to_nearest,			//!< Round towards the nearest value, but exactly-half values are rounded towards maximum magnitude. This mode is the standard school algorithm.
+		near_even, 			//!< Round towards the nearest value, but exactly-half values are rounded towards even values. This mode has more balance than the classic mode.
+		near_odd,			//!< Round towards the nearest value, but exactly-half values are rounded towards odd values. This mode has as much balance as the near_even mode, but preserves more information.
 	};
 } // namespace round
 
@@ -55,30 +59,113 @@ struct raw_representation_tag {};
 //! @brief Undefined value used to specify that "raw" initializing constructor/assignment should be used.
 const raw_representation_tag raw = {};
 
-
+/*!
+ * @brief Implementation of fixed-point number with parametrized word length, number of integer and fractional bits and signedness.
+ * @tparam WordLength total number of bits the number is stored in. Must be one of: 8, 16, 32, 64.
+ * @tparam IntBits number of integer bits. Number of fractional bits is inferred from @p WordLength, @p IntBits and @p sign.
+ * @tparam sign specifies whether this is signed or unsigned number.
+ * @pre Only word lengths that can be represented by built-in integer types are allowed. This is enforced by detail::fixed_word_length_valid compile-time check.
+ * @pre (IntBits <= WordLength - (sign ? 1 : 0)) This is enforced by detail::fixed_int_bits_fit_in_word_length compile-time check.
+ */
 template<unsigned WordLength, unsigned IntBits, bool sign = true>
 class fixed {
 	typedef detail::fixed_rep<WordLength, IntBits, sign> F;
 	typedef typename F::type R;
 	R v_;
+	static const R unity_ = R(1) << F::fractional_bits;
 public:
-	static const unsigned word_length = WordLength;
-	static const unsigned integer_bits = IntBits;
-	static const unsigned fractional_bits = F::fractional_bits;
-	typedef R representation_type;
+	static const unsigned word_length = WordLength;					//!< Well... word length.
+	static const unsigned integer_bits = IntBits;					//!< Number of integer bits.
+	static const unsigned fractional_bits = F::fractional_bits;		//!< Number of fractional bits.
+	typedef R representation_type;									//!< Type of underlying integer representation.
 
+	/*!@brief Default constructor, initializes fixed point number to 0. */
 	fixed(): v_(R()) {}
+	/*!@brief Trivial copy constructor from the same type, simply copy the representation value.
+	 * @param[in] rhs fixed-point number to initialize this to. */
+	fixed(const fixed& rhs): v_(rhs.v_) {}
+	/*!@brief Initialize this fixed-point number with raw integer representation. Useful for interacting with outside-world
+	 * (e.g. using data generated by MATLAB).
+	 * @param[in] v the value to initialize representation with.
+	 */
 	explicit fixed(R v, const raw_representation_tag&): v_(v) {}
 
-//	template<unsigned oth_int, unsigned oth_frac, bool oth_sign>
-//	fixed<oth_int+int_bits, oth_frac+frac_bits, !(sign || oth_sign)> mul(fixed<oth_int, oth_frac, oth_sign> val);
+	//!@todo add dsp::round::mode param and dsp::overflow::mode param, currently both are default (truncated/wrap)
+	explicit fixed(float v): v_(static_cast<R>(unity_ * v)) {}
+	explicit fixed(double v): v_(static_cast<R>(unity_ * v)) {}
+	explicit fixed(long double v): v_(static_cast<R>(unity_ * v)) {}
 
+
+	/*!@brief Obtain raw integer representation.
+	 * @return raw integer representation value. */
 	representation_type raw() const {return v_;}
+	/*!@brief Trivial copy constructor from the same type, simply copy the representation value.
+	 * @param[in] rhs fixed-point number copy.
+	 * @return *this. */
+	fixed& operator=(const fixed& rhs) {v_ = rhs.v_; return *this;}
+
+	friend bool operator==(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ == rhs.v_;}
+	friend bool operator!=(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ != rhs.v_;}
+	friend bool operator<(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ < rhs.v_;}
+	friend bool operator<=(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ <= rhs.v_;}
+	friend bool operator>(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ > rhs.v_;}
+	friend bool operator>=(const fixed<WordLength, IntBits, sign>& lhs, const fixed<WordLength, IntBits, sign>& rhs) {return lhs.v_ > rhs.v_;}
 
 private:
 };
 
-
 }  // namespace dsp
+
+namespace std { // specializing std::numeric_limits for dsp::fixed
+
+template<unsigned WordLength, unsigned IntBits, bool sign>
+class numeric_limits<dsp::fixed<WordLength, IntBits, sign> > {
+	typedef dsp::fixed<WordLength, IntBits, sign> F;
+	typedef typename F::representation_type R;
+	typedef std::numeric_limits<R> NL;
+public:
+
+    static const bool is_specialized = true;
+
+    static F min() throw() {return F(1, dsp::raw);}
+    static F max() throw() {return F(NL::max(), dsp::raw);}
+    static F lowest() throw() {return F(NL::min(), dsp::raw);}
+
+    static const int digits = static_cast<int>(IntBits);
+    static const int digits10 = (int)(digits * 301. / 1000. + .5);
+    static const bool is_signed = sign;
+    static const bool is_integer = false;
+    static const bool is_exact = true;
+    static const int radix = 2;
+
+    static F epsilon() throw() {return F(1, dsp::raw);}
+    static F round_error() throw() {return F(.5f);}
+
+    static const int min_exponent = 0;
+    static const int min_exponent10 = 0;
+    static const int max_exponent = 0;
+    static const int max_exponent10 = 0;
+
+    static const bool has_infinity = false;
+    static const bool has_quiet_NaN = false;
+    static const bool has_signaling_NaN = false;
+	static const float_denorm_style has_denorm = denorm_absent;
+	static const bool has_denorm_loss = false;
+
+    static F infinity() throw() { return F(); }
+    static F quiet_NaN() throw() { return F(); }
+    static F signaling_NaN() throw() { return F(); }
+    static F denorm_min() throw() { return min(); }
+
+    static const bool is_iec559 = false;
+    static const bool is_bounded = true;
+    static const bool is_modulo = false;
+
+    static const bool traps = false;
+    static const bool tinyness_before = false;
+    static const float_round_style round_style = round_toward_zero; // this is the default rounding mode used by parameterless operators
+};
+
+} // namespace std
 
 #endif // DSP_FIXED_H_INCLUDED
