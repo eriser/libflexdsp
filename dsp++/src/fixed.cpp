0cd2cc5b19cc5723578fd36585864a7baf225ad1
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-03 21:05:03 +0000

    [dsp++] work in progress

--- dsp++/src/fixed.cpp
+++ dsp++/src/fixed.cpp
@@ -1,5 +1,7 @@
 #include <dsp++/fixed.h>
 #include <boost/static_assert.hpp>
+#include <climits>
+
 using namespace dsp;
 
 typedef dsp::fixed<16, 0> Q15_t;
@@ -10,7 +12,17 @@ dsp::fixed<8, 3> fff;
 Q15_t v(15, dsp::raw);
 Q15_t v1(16, dsp::raw);
 bool ff = (v == v1);
-Q15_t f(0.00123f);
 
+static bool test1() {
+	FLT_MAX
+	float ff = float((1ull << 15) * 0.001245f);
+	short rr = (short)ff;
+
+	Q15_t f(0.00123f);
+	short r = f.raw();
+	return (r == 0x0028);
+}
+
+bool val = test1();
 
 Q15_t m = std::numeric_limits<Q15_t>::min();
