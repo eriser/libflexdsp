3793b0dcf0ed63fb6e4dd1fded4e0e09b3333d8d
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-04 13:41:32 +0000

    [dsp++] work in progress on fixed-point

--- trunk/dsp++/src/fixed.cpp
+++ trunk/dsp++/src/fixed.cpp
@@ -1,25 +1,27 @@
 #include <dsp++/fixed.h>
 #include <boost/static_assert.hpp>
 
-using namespace dsp;
 
-typedef dsp::fixed<16, 0> Q15_t;
+using namespace dsp::fi;
+
+typedef fixed<16, 0> Q15_t;
+
+Q15_t m = std::numeric_limits<Q15_t>::min();
+
 BOOST_STATIC_ASSERT(Q15_t::fractional_bits == 15);
 
-dsp::fixed<8, 3> fff(0.33333f);
+fixed<8, 3> fff(0.33333f);
 
 Q15_t v(0.33333f);
-Q15_t v1(16, dsp::raw);
+Q15_t v1(16, raw);
 bool ff = (v == v1);
 
 
 static bool test1() {
-	dsp::fixed<32, 12> res = dsp::multiply_lossless(fff, v);
+	fixed<32, 12> res = multiply_lossless<result::max_range>(fff, v);
 
 	return true;
 }
 
 bool val = test1();
-
-Q15_t m = std::numeric_limits<Q15_t>::min();
 
