5e956975a77f8ff52488678193662077630f5304
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-05 22:03:32 +0000

    [dsp++] work in progress on fixed

---
+++ dsp++/include/dsp++/intmath.h
@@ -0,0 +1,259 @@
+/*!
+ * @file dsp++/intmath.h
+ * @brief Integer (and not only) maths helpers. Overflow checking, rounding, basic operations.
+ * @author Andrzej
+ */
+#ifndef DSP_INTMATH_H_INCLUDED
+#define DSP_INTMATH_H_INCLUDED
+#pragma once
+
+#include <limits>
+#include <stdexcept>
+
+namespace dsp {
+
+//! @brief Holds fixed-point (and other) rounding mode constants, so that we don't have name clashes and can nicely qualify values like rounding::fastest.
+namespace rounding {
+	//! @brief Rounding mode.
+	enum mode {
+		fastest,				//!< Speed is more important than the choice in value.
+		negative,				//!< Round towards negative infinity. This mode is useful in interval arithmetic.
+		truncated = fastest,	//!< Round towards zero. This mode is useful in implementing integral arithmetic.
+		positive,				//!< Round towards positive infinity. This mode is useful in interval arithmetic.
+		nearest,				//!< Round towards the nearest value, but exactly-half values are rounded towards maximum magnitude. This mode is the standard school algorithm.
+		near_even, 				//!< Round towards the nearest value, but exactly-half values are rounded towards even values. This mode has more balance than the classic mode.
+		near_odd,				//!< Round towards the nearest value, but exactly-half values are rounded towards odd values. This mode has as much balance as the near_even mode, but preserves more information.
+	};
+} // namespace rounding
+
+//! @brief Holds fixed-point (and other) overflow mode constants, so that we don't have name clashes and can nicely qualify values like overflow::saturate.
+namespace overflow {
+	//! @brief Overflow mode.
+	enum mode {
+		fastest,		//!< Implementation-defined overflow mode (wrap or saturate, if hardware supports saturated maths).
+		wrap = fastest,			//!< Overflowed values will wrap.
+		saturate,		//!< If the dynamic value exceeds the range of the variable, assign the nearest representable value.
+		exception,		//!< If the dynamic value exceeds the range of the variable, throw an exception of type std::overflow_error.
+	};
+} // namespace overflow
+
+namespace detail { // implementation details, not really interesting
+// signum function implementation
+template<class R, bool is_signed = std::numeric_limits<R>::is_signed>
+struct signum_impl; // leave default unimplemented
+
+template<class R> struct signum_impl<R, false> // unsigned specialization - so that we don't get warnings when testing if unsigned values are less than 0
+{static R signum(R val) {return static_cast<R>(val > R());}};
+
+template<class R> struct signum_impl<R, true> // signed specialization
+{static R signum(R val) {return static_cast<R>(R() < val) - (val < R());}};
+
+} // namespace detail
+
+//! @brief Signum (sign) function.
+//! @tparam Type of parameter being tested.
+//! @param[in] val value to test.
+//! @note For unsigned values the only possible results are 0 and 1, therefore the return value maintains type of
+//! the input param so that no additional promotions will be made when the result is used in maths.
+//! @return sign of the value (-1, 0, or 1).
+template<class R>
+inline R signum(R val) {return detail::signum_impl<R>::signum(val);}
+
+namespace detail {
+// overflow handlers, by default silently ignore, this is implementation of overflow::fastest (wrap) mode
+template<class R, overflow::mode OverflowMode>
+struct overflow_impl {static void handle(R&, bool) {}};
+
+template<class R> struct overflow_impl<R, overflow::saturate>
+{static void handle(R& val, bool up) {val = (up ? std::numeric_limits<R>::max() : std::numeric_limits<R>::min());}};
+
+template<class R> struct overflow_impl<R, overflow::exception>
+{static void handle(R&, bool) {throw std::overflow_error("overflow");}};
+
+} // namespace detail
+
+//! @brief Handle overflow detected by higher-level algorithm.
+//! @tparam OverflowMode handling mode
+//! @tparam R type of variable in which where overflow occurred.
+//! @param[in,out] val overflowed value, may be adjusted upon return.
+//! @param[in] up specifies whether overflow occurred in higher or lower direction (overflow/underflow).
+template<overflow::mode OverflowMode, class R>
+inline void handle_overflow(R& val, bool up) {detail::overflow_impl<R, OverflowMode>::handle(val, up);}
+
+namespace detail {
+// overflow-checking add/subtract operations
+template<class R, overflow::mode OverflowMode, bool IsSigned = std::numeric_limits<R>::is_signed> struct add_impl;
+template<class R> struct add_impl<R, overflow::wrap, true> {static R add(R v0, R v1) {return v0 + v1;}};  // specializations for the trivial (wrapping) case
+template<class R> struct add_impl<R, overflow::wrap, false> {static R add(R v0, R v1) {return v0 + v1;}}; // we need both signed and unsigned variants so that we don't get compilation errors due to ambiguous template resolution
+// this specialization covers all non-wrapping unsigned cases
+template<class R, overflow::mode OverflowMode> struct add_impl<R, OverflowMode, false> {
+	static R add(R v0, R v1) {
+		bool ovf = (v1 > std::numeric_limits<R>::max() - v0);
+		v0 += v1;
+		if (ovf)
+			handle_overflow<OverflowMode>(v0, true);
+		return v0;
+	}
+};
+// and this one is for non-wrapping signed cases
+template<class R, overflow::mode OverflowMode> struct add_impl<R, OverflowMode, true> {
+	static R add(R v0, R v1) {
+		bool ovf = false;
+		bool up;
+		if (v0 < R() && v1 < R()) {
+			ovf = (v1 < std::numeric_limits<R>::min() - v0);
+			up = false;
+		}
+		else if (v0 > R() && v1 > R()) {
+			ovf = (v1 > std::numeric_limits<R>::max() - v0);
+			up = true;
+		}
+		v0 += v1;
+		if (ovf)
+			handle_overflow<OverflowMode>(v0, up);
+		return v0;
+	}
+};
+
+template<class R, overflow::mode OverflowMode, bool IsSigned = std::numeric_limits<R>::is_signed> struct sub_impl;
+template<class R> struct sub_impl<R, overflow::wrap, true> {static R sub(R v0, R v1) {return v0 - v1;}};  // specializations for the trivial (wrapping) case
+template<class R> struct sub_impl<R, overflow::wrap, false> {static R sub(R v0, R v1) {return v0 - v1;}}; // we need both signed and unsigned variants so that we don't get compilation errors due to ambiguous template resolution
+// this specialization covers all non-wrapping unsigned cases
+template<class R, overflow::mode OverflowMode> struct sub_impl<R, OverflowMode, false> {
+	static R sub(R v0, R v1) {
+		bool ovf = (v1 > v0);
+		v0 -= v1;
+		if (ovf)
+			handle_overflow<OverflowMode>(v0, true);
+		return v0;
+	}
+};
+// and this one is for non-wrapping signed cases
+template<class R, overflow::mode OverflowMode> struct sub_impl<R, OverflowMode, true> {
+	static R sub(R v0, R v1) {
+		bool ovf = false;
+		bool up;
+		if (v0 < R() && v1 > R()) {
+			ovf = (v1 > v0 - std::numeric_limits<R>::min());
+			up = false;
+		}
+		else if (v0 > R() && v1 < R()) {
+			ovf = (v1 < v0 - std::numeric_limits<R>::max());
+			up = true;
+		}
+		v0 -= v1;
+		if (ovf)
+			handle_overflow<OverflowMode>(v0, up);
+		return v0;
+	}
+};
+
+} // namespace detail
+
+//! @brief Adding with parameterized overflow handling.
+//! @tparam OverflowMode overflow handling constant.
+//! @return (@f${v_0 + v_1}@f$)
+template<overflow::mode OverflowMode, class R>
+inline R add(R v0, R v1) {return detail::add_impl<R, OverflowMode>::add(v0, v1);}
+
+//! @brief Subtracting with parameterized overflow handling.
+//! @tparam OverflowMode overflow handling constant.
+//! @return (@f${v_0 - v_1}@f$).
+template<overflow::mode OverflowMode, class R>
+inline R sub(R v0, R v1) {return detail::add_impl<R, OverflowMode>::add(v0, v1);}
+
+namespace detail {
+//template<class R, bool is_valid = std::numeric_limits<R>::is_integer, bool is_signed = std::numeric_limits<R>::is_signed> struct absint_impl;
+//template<class R> struct absint_impl<R, true, true> {static R absint(R val) {return (val >= 0 ? val : -val);}};
+//template<class R> struct absint_impl<R, true, false> {static R absint(R val) {return val;}};
+//template<class R> inline R absint(R val) {return absint_impl<R>::absint(val);}
+
+// overflow-checking rounding of integer values at specified bit according to rounding mode
+template<class R, rounding::mode RoundMode, overflow::mode OverflowMode, bool IsSigned = std::numeric_limits<R>::is_signed> struct round_impl;
+// discard all the bits below the specified one, validity of at_bit is checked by the driving function
+template<class R, overflow::mode OverflowMode, bool IsSigned> struct round_impl<R, rounding::truncated, OverflowMode, IsSigned> {
+	static R round(R val, int at_bit) {
+		R div = R(1 << at_bit);
+		R r = val % div;
+		val -= r;			// this will never overflow
+		return val;
+	}
+};
+
+template<class R, overflow::mode OverflowMode> struct round_impl<R, rounding::nearest, OverflowMode, false> {
+	static R round(R val, int at_bit) {
+		R div = R(1 << at_bit);
+		R r = val % div;
+		if (0 == r)
+			return val;
+		val -= r;
+		if (r >= div / 2)
+			val = add<OverflowMode>(val, div);
+		return val;
+	}
+};
+
+template<class R, overflow::mode OverflowMode> struct round_impl<R, rounding::nearest, OverflowMode, true> {
+	static R round(R val, int at_bit) {
+		R div = R(1 << at_bit);
+		R r = val % div;
+		if (0 == r)
+			return val;
+		val -= r;
+		if (r >= div / 2)
+			val = add<OverflowMode>(val, div);
+		else if (r < -div / 2)
+			val = sub<OverflowMode>(val, div);
+		return val;
+	}
+};
+
+
+template<class R, overflow::mode OverflowMode, bool IsSigned> struct round_impl<R, rounding::positive, OverflowMode, IsSigned> {
+	static R round(R val, int at_bit) {
+		R div = R(1 << at_bit);
+		R r = val % div;
+		if (0 == r)
+			return val;
+		val -= r;
+		if (r > 0)
+			val = add<OverflowMode>(val, div);
+		return val;
+	}
+};
+
+// for unsigned numbers rounding towards -inf is the same as truncating
+template<class R, overflow::mode OverflowMode> struct round_impl<R, rounding::negative, OverflowMode, false>: public round_impl<R, rounding::truncated, OverflowMode, false> {};
+
+template<class R, overflow::mode OverflowMode> struct round_impl<R, rounding::negative, OverflowMode, true> {
+	static R round(R val, int at_bit) {
+		R div = R(1 << at_bit);
+		R r = val % div;
+		if (0 == r)
+			return val;
+		val -= r;
+		if (r < 0)
+			val = sub<OverflowMode>(val, div);
+		return val;
+	}
+};
+
+} // namespace detail
+
+template<rounding::mode RoundMode, overflow::mode OverflowMode, class R>
+inline R round(R val, int at_bit) {
+	if (at_bit <= 0)
+		return val;
+	else if (at_bit >= std::numeric_limits<R>::digits && val != 0) {
+		bool up = (val > 0);
+		val = 0;
+		handle_overflow<OverflowMode>(val, up);
+	}
+	else
+		val = detail::round_impl<R, RoundMode, OverflowMode>::round(val, at_bit);
+	return val;
+}
+
+} // namespace dsp
+
+#endif /* DSP_INTMATH_H_INCLUDED */
