eb1217b93156854ba4e8727e6013be9ca69fc0d1
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-05 13:22:25 +0000

    [dsp++] work in progress on fixed

--- dsp++/src/fixed.cpp
+++ dsp++/src/fixed.cpp
@@ -13,12 +13,16 @@ BOOST_STATIC_ASSERT(Q15_t::fractional_bits == 15);
 fixed<8, 3> fff(-0.33333f);
 
 Q15_t v(0.33333);
-Q15_t v1(16, raw);
+Q15_t v1(2048, raw);
 bool ff = (v == v1);
 
 
 static bool test1() {
 	fixed<32, 12> res = multiply_lossless<result::max_range>(fff, v);
+	fixed<32, 4> res1 = multiply_lossless<result::max_precision>(fff, v);
+	float ff = float_cast<float>(res);
+	float rr = float_cast<float>(res1);
+
 	fixed<32, 12> r0 = res.round<round::truncated>(5);
 	float f0 = float_cast<float>(r0);
 	fixed<32, 12> r1 = res.round<round::nearest>(5);
@@ -28,7 +32,10 @@ static bool test1() {
 	fixed<32, 12> r3 = res.round<round::negative>(5);
 	float f3 = float_cast<float>(r3);
 
-	Q15_t q = v * v1;
+	float fv1 = float_cast<float>(v1);
+	fv1 *= 0.33333;
+
+	fixed<16, 0> q = v * v1;
 	float fq = float_cast<float>(q);
 	return true;
 }
@@ -37,10 +44,10 @@ bool val = test1();
 
 static bool test2() {
 	for (short i = SHRT_MIN; i < SHRT_MAX; ++i) {
-		short s0 = detail::round_impl<short, round::truncated>::round(i, 5);
-		short s1 = detail::round_impl<short, round::nearest>::round(i, 5);
-		short s2 = detail::round_impl<short, round::positive>::round(i, 5);
-		short s3 = detail::round_impl<short, round::negative>::round(i, 5);
+		short s0 = detail::round_impl<short, round::truncated, overflow::fastest>::round(i, 5);
+		short s1 = detail::round_impl<short, round::nearest, overflow::exception>::round(i, 5);
+		short s2 = detail::round_impl<short, round::positive, overflow::saturate>::round(i, 5);
+		short s3 = detail::round_impl<short, round::negative, overflow::wrap>::round(i, 5);
 	}
 	return true;
 }
