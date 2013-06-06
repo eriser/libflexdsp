96e7a4b92081f870d22a2147b11c910b51e4316a
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2013-06-06 12:31:55 +0000

    [dsp++-test] added unit tests for intmath ops

--- trunk/dsp++/src/fixed.cpp
+++ trunk/dsp++/src/fixed.cpp
@@ -5,13 +5,8 @@
 using namespace dsp::fi;
 
 typedef fixed<16, 0> Q15_t;
-
-Q15_t m = std::numeric_limits<Q15_t>::min();
-
 BOOST_STATIC_ASSERT(Q15_t::fractional_bits == 15);
-
 fixed<8, 3> fff(-0.33333f);
-
 Q15_t v(0.33333);
 Q15_t v1(2048, raw);
 bool ff = (v == v1);
@@ -23,14 +18,14 @@ static bool test1() {
 	float ff = float_cast<float>(res);
 	float rr = float_cast<float>(res1);
 
-	fixed<32, 12> r0 = res.round<dsp::rounding::fastest>(5);
-	float f0 = float_cast<float>(r0);
-	fixed<32, 12> r1 = res.round<dsp::rounding::nearest>(5);
-	float f1 = float_cast<float>(r1);
-	fixed<32, 12> r2 = res.round<dsp::rounding::positive>(5);
-	float f2 = float_cast<float>(r2);
-	fixed<32, 12> r3 = res.round<dsp::rounding::negative>(5);
-	float f3 = float_cast<float>(r3);
+////	fixed<32, 12> r0 = res.round<dsp::rounding::fastest>(5);
+//	float f0 = float_cast<float>(r0);
+////	fixed<32, 12> r1 = res.round<dsp::rounding::nearest>(5);
+//	float f1 = float_cast<float>(r1);
+//	fixed<32, 12> r2 = res.round<dsp::rounding::positive>(5);
+//	float f2 = float_cast<float>(r2);
+//	fixed<32, 12> r3 = res.round<dsp::rounding::negative>(5);
+//	float f3 = float_cast<float>(r3);
 
 	float fv1 = float_cast<float>(v1);
 	fv1 *= 0.33333;
