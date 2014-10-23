65fc4fece000c26d24b859d13d0d6e837cb12c99
andrzej.ciarkowski@gmail.com <andrzej.ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2014-10-23 11:58:17 +0000

    [dsp++] fixed transforming of mkfilter output in iir_filter_design

--- dsp++-test/src/filter_design_test.h
+++ dsp++-test/src/filter_design_test.h
@@ -17,12 +17,14 @@ namespace dsp { namespace test {
 		CPPUNIT_TEST(test_bp);
 		CPPUNIT_TEST(test_hilbert);
 		CPPUNIT_TEST(test_differentiator);
+		CPPUNIT_TEST(test_butter_bp);
 		CPPUNIT_TEST_SUITE_END();
 
 	public:
 		void test_bp();
 		void test_hilbert();
 		void test_differentiator();
+		void test_butter_bp();
 	};
 
 } } /* namespace dsp::test */
