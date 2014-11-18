9dfcb06418b6649bcc126ea8d69e96bca94a16bf
andrzej.ciarkowski@gmail.com <andrzej.ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2014-11-18 11:02:44 +0000

    [dsp++] FDAF seems to be working fine

--- dsp++/include/dsp++/fdaf.h
+++ dsp++/include/dsp++/fdaf.h
@@ -41,29 +41,33 @@ public:
 
 
 	~fdaf_overlap_save() {
-		calloc_.deallocate(cbuf_, 6*N_);
-		ralloc_.deallocate(rbuf_, 6*N_);
+		calloc_.deallocate(cbuf_, 8*N_);
+		ralloc_.deallocate(rbuf_, 8*N_);
 	}
 
-	fdaf_overlap_save(size_t block_length, value_type mu, value_type leakage = value_type(1))
+	fdaf_overlap_save(size_t block_length, value_type mu, value_type leakage = value_type(1), value_type init_pow = value_type(1), value_type avg_fact = value_type(0.9), value_type offset = value_type())
 	 :	N_(block_length)
-	 ,	rbuf_(ralloc_.allocate(6*N_))
-	 ,	cbuf_(calloc_.allocate(6*N_))
+	 ,	rbuf_(ralloc_.allocate(8*N_))
+	 ,	cbuf_(calloc_.allocate(8*N_))
 	 ,	dft_(2*N_, rbuf_, cbuf_)
 	 ,	idft_(2*N_, cbuf_, rbuf_)
 	 ,	x_(rbuf_)
-	 ,	e_(x_ + 2*N_)
-	 ,	y_(e_ + 2*N_)
+	 ,	y_(x_ + 2*N_)
+	 ,	w_(y_ + 2*N_)
+	 ,	e_(w_ + 2*N_)
 	 ,	d_(e_ + N_)
-	 ,	v_(e_)
 	 ,	X_(cbuf_)
 	 ,	E_(X_ + 2*N_)
 	 ,	W_(E_ + 2*N_)
+	 ,	norm_(W_ + 2*N_)
 	 ,	mu_(mu)
 	 ,	lambda_(leakage)
+	 ,	offset_(offset)
+	 ,	beta_(avg_fact)
 	{
-		std::fill_n(rbuf_, 6*N_, value_type());
+		std::fill_n(rbuf_, 8*N_, value_type());
 		std::fill_n(cbuf_, 6*N_, complex_type());
+		std::fill_n(norm_, 2*N_, complex_type(init_pow));
 	}
 
 	iterator x_begin() {return x_ + N_;}
@@ -73,11 +77,13 @@ public:
 	std::pair<iterator, iterator> x() {return std::make_pair(x_begin(), x_end());}
 	std::pair<const_iterator, const_iterator> x() const {return std::make_pair(x_begin(), x_end());}
 
-	//iterator y_begin() {return y_;}
-	const_iterator y_begin() const {return y_;}
-	//iterator y_end() {return y_ + N_;}
-	const_iterator y_end() const {return y_ + N_;}
+	const_iterator y_begin() const {return y_ + N_;}
+	const_iterator y_end() const {return y_ + 2*N_;}
 	std::pair<const_iterator, const_iterator> y() const {return std::make_pair(y_begin(), y_end());}
+
+	const_iterator e_begin() const {return e_;}
+	const_iterator e_end() const {return e_ + N_;}
+	std::pair<const_iterator, const_iterator> e() const {return std::make_pair(e_begin(), e_end());}
 
 	iterator d_begin() {return d_;}
 	const_iterator d_begin() const {return d_;}
@@ -95,37 +101,47 @@ public:
 
 	//! @return Step size \f$\mu\f$ of the LMS algorithm.
 	value_type step_size() const {return mu_;}
-	//! @brief Modify step size \f$\mu\f$ of the LMS algorithm. @param[in] mu new step size used during subsequent iterations.
-	void set_step_size(const value_type mu) {mu_ = mu;}
 	//! @return Step size \f$\mu\f$ of the LMS algorithm.
 	value_type mu() const {return mu_;}
+	//! @brief Modify step size \f$\mu\f$ of the LMS algorithm. @param[in] mu new step size used during subsequent iterations.
+	void set_step_size(const value_type mu) {mu_ = mu;}
 	//! @brief Modify step size \f$\mu\f$ of the LMS algorithm. @param[in] mu new step size used during subsequent iterations.
 	void set_mu(const value_type mu) {mu_ = mu;}
 
 	value_type leakage() const {return lambda_;}
 	void set_leakage(const value_type lambda) {lambda_ = lambda;}
 
+	value_type offset() const {return offset_;}
+	void set_offset(value_type offset) {offset_ = offset;}
+
+	value_type averaging_factor() const {return beta_;}
+	void set_averaging_factor(value_type beta) {beta_ = beta;}
+
 	void operator()() {
-		dft_(x_, X_);													// calculate FFT of x() and put it into X
-		std::transform(X_, X_ + 2*N_, W_, E_, std::multiplies<complex_type>());			// y = IFFT{X * W}, using E_ as temporary variable
+		dft_(x_, X_);															// X = FFT{x()}
+		std::transform(X_, X_ + 2*N_, W_, E_, std::multiplies<complex_type>());	// y() = IFFT{X * W}, using E_ as temporary variable
 		idft_(E_, y_);						
-		std::transform(y_ + N_, y_ + 2*N_, y_ + N_, std::bind2nd(std::divides<value_type>(), 2*N_)); // scale IFFT and store output into 2nd half of y
-		std::transform(d_, d_ + N_, y_ + N_, d_, std::minus<value_type>());	// subtract y() from d() to form 2nd half of e()
-		std::transform(e_ + N_, e_ + 2*N_, e_ + N_, std::bind2nd(std::multiplies<value_type>(), mu_));	// apply step_size e() = e() * mu
-		std::fill_n(e_, N_, value_type());								// zero first half of e()
-		dft_(e_, E_);													// put FFT of zero-prepended e() into E
-		complex_iterator E = E_, X = X_;
-		for (size_t i = 0; i < 2*N_; ++i, ++E, ++X)						// multiply F{e()} with conjugate copy of F{x()} (no functor for 2 operations at once?)
-			(*E) *= std::conj(*X);
-		idft_(E_, v_);													// perform IFFT on multiplication result, calculate gradient constraint
-		std::transform(v_, v_ + N_, v_, std::bind2nd(std::divides<value_type>(), 2*N_));		// scale IFFT output, but only for the useful half
-		std::fill_n(v_ + N_, N_, value_type());							// discard and zero-fill 2nd half of v()
-		dft_(v_, E_);													// FFT v() back to DFT domain
-		complex_iterator W = W_; E = E_, X = X_;
-		for (size_t i = 0; i < 2*N_; ++i, ++W, ++E, ++X) {				// update W with forgetting-factor-multiplied error transform
-			*W *= lambda_;
-			*W += (*E);													// W(k + 1) = W(k) + 2uF{v(n)}
+		std::transform(y_ + N_, y_ + 2*N_, y_ + N_, std::bind2nd(std::divides<value_type>(), 2*N_)); // scale IFFT (only applied to 2nd half of y_, 1st one is irrelevant)
+		std::transform(d_, d_ + N_, y_ + N_, e_, std::minus<value_type>());		// e() = d() - y(), e_ is now ready
+		std::transform(e_, e_ + N_, w_ + N_, std::bind2nd(std::multiplies<value_type>(), mu_));	// apply step_size e() = e() * mu, put result in 2nd half of w_
+		std::fill_n(w_, N_, value_type());								// zero first half of w_
+		dft_(w_, E_);													// put FFT of zero-prepended e() into E
+
+		value_type ombet = value_type(1) - beta_;
+		complex_iterator E = E_, N = norm_, X = X_;
+		for (size_t i = 0; i < 2*N_; ++i, ++E, ++N, ++X) {			
+			complex_type cX = std::conj(*X);
+			(*N *= beta_) += ombet * std::real(*X * cX);	// update signal power 
+			(*E) *= (cX / (*N + offset_));					// multiply E with conjugate of X and normalize
 		}
+		idft_(E_, w_);													// perform IFFT on multiplication result, calculate gradient constraint
+		std::transform(w_, w_ + N_, w_, std::bind2nd(std::divides<value_type>(), 2*N_));	// scale IFFT output, but only for the useful half
+		std::fill_n(w_ + N_, N_, value_type());							// discard and zero-fill 2nd half of v()
+		dft_(w_, E_);													// FFT v() back to DFT domain
+		complex_iterator W = W_; E = E_;
+		for (size_t i = 0; i < 2*N_; ++i, ++W, ++E) 					// update W with forgetting-factor-multiplied error transform
+			(*W *= lambda_) += *E;
+
 		std::copy(x_ + N_, x_ + 2*N_, x_);								// store current x input in the 1st half of x as "previous" frame
 	}
 
@@ -141,17 +157,19 @@ private:
 	inverse_transform_type idft_;	//!< IDFT functor
 
 	value_type* const x_;	//!< (2N) input vector, x[0..N) is old (saved) input frame frame, x[N..2N) is the new input frame upon input 
-	value_type* const e_;	//!< (2N) error input vector, e[0..N) will be filled with 0s, e[N..2N) is d() - y()
 	value_type* const y_;	//!< (2N) output vector 
-	value_type* const d_;	//!< (N) desired response input vector, this is convenience as it will point to the 2nd half of e_
-	value_type* const v_;	//!< (2N) gradient constraint calculation vector, this is convenience only as e_ will be reused
+	value_type* const w_;	//!< (2N) real-valued work area
+	value_type* const e_;	//!< (N) error output vector, e() = d() - y()
+	value_type* const d_;	//!< (N) desired response input vector
 	complex_type* const X_;	//!< (2N) FFT(x()) output
 	complex_type* const E_;	//!< (2N) FFT(e()) output
 	complex_type* const W_; //!< (2N) adaptive weights transform
+	complex_type* const norm_;	//!< (2N) signal power for normalization
 
 	value_type mu_;				//!< step size mu
 	value_type lambda_;			//!< leakage factor 
-
+	value_type offset_;			//!< normalization offset to avoid divide by zero
+	value_type beta_;			//!< averaging factor for exponential averaning of input power
 };
 
 }
