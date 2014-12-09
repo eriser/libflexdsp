caa8c7ff142726eb96630726bf9a3bf62251977f
andrzej.ciarkowski@gmail.com <andrzej.ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2014-12-09 09:05:24 +0000

    [dsp++] additional test for OLA

--- trunk/dsp++-test/src/overlap_add_test.h
+++ trunk/dsp++-test/src/overlap_add_test.h
@@ -19,10 +19,12 @@ class overlap_add_test: public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE(overlap_add_test);
 	CPPUNIT_TEST(test_ola);
 	CPPUNIT_TEST(test_ols);
+	//CPPUNIT_TEST(test_ola_long_csv);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void test_ola();
 	void test_ols();
+	void test_ola_long_csv();
 };
 
 } /* namespace test */
