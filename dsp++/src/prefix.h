5111e256969e989f3a23c87cf31eb97c80ef78d3
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2014-12-19 23:50:51 +0000

    [dsp++] added xcode prefix file

---
+++ dsp++/src/prefix.h
@@ -0,0 +1,33 @@
+#ifdef __cplusplus
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cfloat>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+#include <cstring>
+#include <cstddef>
+#include <cstdlib>
+#include <memory>
+
+#ifndef DSP_BOOST_DISABLED
+#include <boost/smart_ptr.hpp>
+#include <boost/noncopyable.hpp>
+
+#endif
+
+#endif
+
+#ifndef DSP_FFTW_DISABLED
+#include <fftw3.h>
+#endif
+
+#ifndef DSP_SNDFILE_DISABLED
+#include <sndfile.h>
+#endif
