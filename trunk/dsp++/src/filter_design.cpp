35de3553d17aee476981ac47f611af859d72870a
Andrzej.Ciarkowski@gmail.com <Andrzej.Ciarkowski@gmail.com@2595aa05-122a-0531-86cc-3bafd8832c48>
2014-12-19 16:50:11 +0000

    [dsp++] cleaning the mess with filter_design

--- trunk/dsp++/src/filter_design.cpp
+++ trunk/dsp++/src/filter_design.cpp
@@ -4,7 +4,7 @@
  */
 #include <dsp++/filter_design.h>
 #include "remez/remez.h"
-#include "mkfilter/mkfltif.h"
+#include "mkfilter/mkfilter.h"
 
 #include <stdexcept>
 #include <cmath>
@@ -47,7 +47,7 @@ bool dsp::firpm(size_t order, double h[], size_t band_count, double freqs[], con
 	return (REMEZ_NOERR == res);
 }
 
-void dsp::biquad_design(double b[], double a[], biquad_type type, double norm_freq, const double* gain_db, const double* q, const double* bw, const double* s)
+void dsp::biquad::design(double b[], double a[], biquad::type::spec type, double norm_freq, const double* gain_db, const double* q, const double* bw, const double* s)
 {
 	if (norm_freq < 0. || norm_freq > 0.5)
 		throw std::domain_error("dsp::biquad_design(): norm_freq outside [0, 0.5]");
@@ -64,9 +64,9 @@ void dsp::biquad_design(double b[], double a[], biquad_type type, double norm_fr
 		sqAa2 = 0;	// 2 * sqrt(A) * alpha
 	bool is_eq;
 	switch (type) {
-	case biquad_type_peaking_eq:
-	case biquad_type_low_shelf_eq:
-	case biquad_type_high_shelf_eq:
+	case biquad::type::peaking_eq:
+	case biquad::type::low_shelf_eq:
+	case biquad::type::high_shelf_eq:
 		if (NULL == gain_db)
 			throw std::invalid_argument("dsp::biquad_design(): gain_db not specified");
 		A = std::pow(10., *gain_db / 40.);
@@ -93,35 +93,35 @@ void dsp::biquad_design(double b[], double a[], biquad_type type, double norm_fr
 
 	double n2c = -2 * cw0;	// -2 * cos(w0)
 	switch (type) {
-	case biquad_type_lowpass:
+	case biquad::type::lowpass:
 		b[0] = (1 - cw0) / 2; b[1] = 1 - cw0; b[2] = b[0];
 		a[0] = 1 + alpha; a[1] = n2c; a[2] = 1 - alpha;
 		break;
-	case biquad_type_highpass:
+	case biquad::type::highpass:
 		b[0] = (1 + cw0) / 2; b[1] = -(1 + cw0); b[2] = b[0];
 		a[0] = 1 + alpha; a[1] = n2c; a[2] = 1 - alpha;
 		break;
-	case biquad_type_bandpass:
+	case biquad::type::bandpass:
 		b[0] = alpha; b[1] = 0; b[2] = -alpha;
 		a[0] = 1 + alpha; a[1] = n2c; a[2] = 1 - alpha;
 		break;
-	case biquad_type_notch:
+	case biquad::type::notch:
 		b[0] = 1; b[1] = n2c; b[2] = 1;
 		a[0] = 1 + alpha; a[1] = n2c; a[2] = 1 - alpha;
 		break;
-	case biquad_type_allpass:
+	case biquad::type::allpass:
 		b[0] = 1 - alpha; b[1] = n2c; b[2] = 1 + alpha;
 		a[0] = b[2]; a[1] = n2c; a[2] = b[0];
 		break;
-	case biquad_type_peaking_eq:
+	case biquad::type::peaking_eq:
 		b[0] = 1 + alpha * A; b[1] = n2c; b[2] = 1 - alpha * A;
 		a[0] = 1 + alpha / A; a[1] = n2c; a[2] = 1 - alpha / A;
 		break;
-	case biquad_type_low_shelf_eq:
+	case biquad::type::low_shelf_eq:
 		b[0] = A * (Ap1 - Am1c + sqAa2); b[1] = 2 * A * (Am1 - Ap1c); b[2] = A * (Ap1 - Am1c - sqAa2);
 		a[0] = (Ap1 + Am1c + sqAa2); a[1] = -2 * (Am1 + Ap1c); a[2] = Ap1 + Am1c - sqAa2;
 		break;
-	case biquad_type_high_shelf_eq:
+	case biquad::type::high_shelf_eq:
 		b[0] = A * (Ap1 + Am1c + sqAa2); b[1] = -2 * A * (Am1 + Ap1c); b[2] = A * (Ap1 + Am1c - sqAa2);
 		a[0] = (Ap1 - Am1c + sqAa2); a[1] = 2 * (Am1 - Ap1c); a[2] = Ap1 - Am1c - sqAa2;
 		break;
@@ -135,14 +135,14 @@ static size_t init_mkfilter(mkfilter::context& ctx, size_t order, unsigned type,
 	size_t sz = order;
 	const unsigned type_mask = 0x0003;
 	switch (type & type_mask) {
-	case iir_bessel: 
+	case iir::type::bessel: 
 		ctx.options |= mkfilter::opt_be; 
 		break;
-	case iir_chebyshev: 
+	case iir::type::chebyshev: 
 		ctx.options |= mkfilter::opt_ch; 
 		ctx.chebrip = (NULL == cheb_rip ? 3. : *cheb_rip);
 		break;
-	case iir_butterworth: 
+	case iir::type::butterworth: 
 		ctx.options |= mkfilter::opt_bu; 
 		break;
 	default:
@@ -151,27 +151,27 @@ static size_t init_mkfilter(mkfilter::context& ctx, size_t order, unsigned type,
 
 	const unsigned char_mask = 0x01f0;
 	switch (type & char_mask) {
-	case iir_lowpass: 
+	case iir::resp::lowpass: 
 		ctx.options |= mkfilter::opt_lp | mkfilter::opt_a;
 		ctx.raw_alpha1 = *fc;
 		break;
-	case iir_highpass:
+	case iir::resp::highpass:
 		ctx.options |= mkfilter::opt_hp | mkfilter::opt_a;
 		ctx.raw_alpha1 = *fc;
 		break;
-	case iir_bandpass:
+	case iir::resp::bandpass:
 		ctx.options |= mkfilter::opt_bp | mkfilter::opt_a;
 		ctx.raw_alpha1 = fc[0];
 		ctx.raw_alpha2 = fc[1];
 		sz = 2 * order;
 		break;
-	case iir_bandstop:
+	case iir::resp::bandstop:
 		ctx.options |= mkfilter::opt_bs | mkfilter::opt_a;
 		ctx.raw_alpha1 = fc[0];
 		ctx.raw_alpha2 = fc[1];
 		sz = 2 * order;
 		break;
-	case iir_allpass:
+	case iir::resp::allpass:
 		ctx.options |= mkfilter::opt_ap | mkfilter::opt_a;
 		ctx.raw_alpha1 = *fc;
 		break;
@@ -187,9 +187,9 @@ static size_t init_mkfilter(mkfilter::context& ctx, size_t order, unsigned type,
 		ctx.options |= mkfilter::opt_p;
 		ctx.polemask = pole_mask;
 	}
-	if (type & iir_prewrap)
+	if (type & iir::flag::prewrap)
 		ctx.options |= mkfilter::opt_w;
-	if (type & iir_matched_z)
+	if (type & iir::flag::matched_z)
 		ctx.options |= mkfilter::opt_z;
 
 	ctx.order = static_cast<int>(order);
@@ -203,7 +203,7 @@ static size_t init_mkfilter(mkfilter::context& ctx, size_t order, unsigned type,
 }
 
 
-void dsp::iir_filter_design(size_t order, double b[], double a[], unsigned type, const double* fc, const double* cheb_rip, const double* zero_freq, unsigned pole_mask)
+void dsp::iir::design(size_t order, double b[], double a[], unsigned type, const double* fc, const double* cheb_rip, const double* zero_freq, unsigned pole_mask)
 {
 	mkfilter::context ctx;
 	memset(&ctx, 0, sizeof(ctx));
@@ -213,8 +213,8 @@ void dsp::iir_filter_design(size_t order, double b[], double a[], unsigned type,
 
 	mkfilter::design(ctx);
 
-	assert(ctx.splane.numzeros <= sz);
-	assert(ctx.splane.numpoles <= sz);
+	assert(ctx.splane.numzeros <= (int)sz);
+	assert(ctx.splane.numpoles <= (int)sz);
 	assert(ctx.zplane.numzeros == sz);
 	assert(ctx.zplane.numpoles == sz);
 	// inverse coeffs order - mkfilter produces vectors in the format for its own internal filter implementation which uses inversed vectors
@@ -225,7 +225,7 @@ void dsp::iir_filter_design(size_t order, double b[], double a[], unsigned type,
 	}
 }
 
-double dsp::iir_filter_design(size_t order,	std::complex<double> z[], std::complex<double> p[],	unsigned type, 
+double dsp::iir::design(size_t order,	std::complex<double> z[], std::complex<double> p[],	unsigned type, 
 	const double* fc, const double* cheb_rip, const double* zero_freq, unsigned pole_mask)
 {
 	mkfilter::context ctx;
@@ -234,8 +234,8 @@ double dsp::iir_filter_design(size_t order,	std::complex<double> z[], std::compl
 
 	mkfilter::design(ctx);
 
-	assert(ctx.splane.numzeros <= sz);
-	assert(ctx.splane.numpoles <= sz);
+	assert(ctx.splane.numzeros <= (int)sz);
+	assert(ctx.splane.numpoles <= (int)sz);
 	assert(ctx.zplane.numzeros == sz);
 	assert(ctx.zplane.numpoles == sz);
 	std::copy(ctx.zplane.zeros, ctx.zplane.zeros + sz, z);
@@ -253,7 +253,7 @@ static bool test_iir() {
 	//iir_filter_design(10, b, a, dsp::iir_bandpass, fc);
 
 	std::complex<double> z[20], p[20];
-	double k = iir_filter_design(4, z, p, dsp::iir_highpass, fc);
+	double k = iir::design(4, z, p, iir::resp::bandstop, fc);
 	return true;
 }
 
